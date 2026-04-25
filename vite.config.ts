0f911e72fdaa343cfbe895f8196576305e21e59a
Alberto Fernandez <albertofernandezmoya2@.com>
2026-04-25 17:12:41 +0200

    fix: configurar base path para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/albertoweb/", // <- Esto es clave para GitHub Pages
   server: {
     host: "::",
     port: 8080,
@@ -17,6 +18,6 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
   },
 }));
