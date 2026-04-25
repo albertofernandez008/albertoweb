d4ee668958353074df6796f1f21c58cdd0cb21d3
Alberto Fernandez <albertofernandezmoya2@.com>
2026-04-25 17:37:16 +0200

    fix: nuevo workflow con GitHub Actions

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <BrowserRouter basename="/becquer/">
+  <Routes>
+    <Route path="" element={<Index />} />          {/* aquí */}
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
