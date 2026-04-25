73ce78cabbc1a6e3ec7ed0b94fd203359eb1cb80
Alberto Fernandez <albertofernandezmoya2@.com>
2026-04-25 18:38:07 +0200

    restaurar index completo

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
-import React from "react";
-
-class ErrorBoundary extends React.Component<{children: React.ReactNode}, {error: any}> {
-  constructor(props: any) {
-    super(props);
-    this.state = { error: null };
-  }
-  componentDidCatch(error: any) {
-    this.setState({ error });
-  }
-  render() {
-    if (this.state.error) {
-      return <div style={{color:'red',padding:'20px'}}>{String(this.state.error)}</div>;
-    }
-    return this.props.children;
-  }
-}
 
 const queryClient = new QueryClient();
 const App = () => (
@@ -29,13 +12,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/albertoweb/">
-        <ErrorBoundary>
-          <Routes>
-            <Route path="" element={<Index />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </ErrorBoundary>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
