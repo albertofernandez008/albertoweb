e3aab7b1b0890585f4004197b8539bf186883272
Alberto Fernandez <albertofernandezmoya2@.com>
2026-04-25 17:24:28 +0200

    fix: base path dinámico para dev y producción

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/albertoweb/", // <- Esto es clave para GitHub Pages
+  base: mode === "production" ? "/albertoweb/" : "/",
   server: {
     host: "::",
     port: 8080,
